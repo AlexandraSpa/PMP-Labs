ec3c8b923fe5217fc9e40cf51c428da094a45a91
AlexandraSpa <58995060+AlexandraSpa@users.noreply.github.com>
2021-12-06 15:53:44 +0200

    Exercitiul 1

--- src/main/scala/Lab8/Lab8.scala
+++ src/main/scala/Lab8/Lab8.scala
@@ -1,7 +1,6 @@
 package Lab8
 
 import com.cra.figaro.library.atomic.continuous
-import com.cra.figaro.library.atomic.discrete._
 import com.cra.figaro.library.collection.Container
 import com.cra.figaro.algorithm.factored.VariableElimination
 import com.cra.figaro.language._
@@ -14,10 +13,7 @@ object Ex1 {
     val learned: Array[Element[Boolean]] =
     Array.fill(length)(Constant(false))
 
-    val score: Array[Element[Int]] =
-    Array.fill(length)(FromRange(1, 11))
-
-    val test: Array[Element[Boolean]] =
+    val score: Array[Element[Boolean]] =
     Array.fill(length)(Constant(false))
 
     learned(0) = Flip(0.6)
@@ -28,24 +24,12 @@ object Ex1 {
 
     for { chapter <- 0 until length }
     {
-      score(chapter) = If(learned(chapter), FromRange(5, 11), FromRange(1, 5))
+      score(chapter) = If(learned(chapter), Flip(0.7), Flip(0.3))
     } //
 
-    for { chapter <- 0 until length }
-    {
-       test(chapter) = Apply(score(chapter),
-        (s: Int) => 
-        if(s > 4) true
-        else false
-      )
-       //If((score(chapter) > 4), Constant(true), Constant(false))
-    } 
-
-    score(0).observe(6)
-    score(1).observe(7)
-    score(2).observe(8)
-    println("Probability that the student will pass the last test successfull: " + VariableElimination.probability(test(9), true))
-    score(8).observe(4)
-    println("Probability that the student will pass the last test successfull, given the score of test no. 9 = 4: " + VariableElimination.probability(test(9), true))
+    score(0).observe(true)
+    score(1).observe(true)
+    score(2).observe(true)
+    println("Probability that the student will pass the last test successfull: " + VariableElimination.probability(score(9), true))
   }
 }
